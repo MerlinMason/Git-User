c38f3cb70b392afa928e12f3f0632c0cc1a66ac2
Merlin Mason <info@merlinmason.co.uk>
2014-05-02 20:28:13 +0100

    hit api and spit out a template on success

--- js/project.js
+++ js/project.js
@@ -3,18 +3,48 @@
 
     var gituser = {
 
+        templateurl:     window.location.href + "/templates/",
+        api: "https://api.github.com/users/",
+        cache: {},
+
         init: function () {
             this.bindUIActions();
         },
 
         bindUIActions: function () {
-            $(".btn").on("click", function (e) { gituser.sayHello(e); });
+            $("#getuser").on("submit", function (e) { gituser.getUser(e); });
         },
 
-        sayHello: function (e) {
-            var button = $(e.currentTarget);
-            alert("Hello from " + button);
-        }
+        getUser: function (e) {
+            e.preventDefault();
+            var query = $("#getuser .username").val();
+
+            if (query.length) {
+                $.when(
+                    $.get(gituser.api + query, function (user) {
+                        gituser.cache.user = user;
+                    }),
+                    $.get(gituser.api + query + "/repos", function (repos) {
+                        gituser.cache.repos = repos;
+                    })
+                ).then(function () {
+                    console.log(gituser.cache);
+                    gituser.renderView(gituser.cache, "result.html", "#result", false);
+                });
+            }
+        },
+
+        renderView: function (data, template, el, callback) {
+            $.get(gituser.templateurl + template, function (template) {
+                $(el).html(
+                    _.template(template, data)
+                );
+
+                $(el).fadeIn("slow");
+
+                if (callback) { callback(); }
+            });
+        },
 
     };
 
