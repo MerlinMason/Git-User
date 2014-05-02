15ac12241767e68991ba52bdcab8b152a88a3f16
Merlin Mason <info@merlinmason.co.uk>
2014-05-02 23:21:20 +0100

    improve error handling and UX

--- js/project.js
+++ js/project.js
@@ -6,6 +6,7 @@
         templateurl:     window.location.href + "/templates/",
         api: "https://api.github.com/users/",
         cache: {},
+        errors: { messages: [] },
 
         init: function () {
             this.bindUIActions();
@@ -17,49 +18,74 @@
 
         getUser: function (e) {
             e.preventDefault();
-            var query = $("#getuser .username").val();
+            var input = $("#getuser .username");
+            var query = $(input).val();
 
             if (query.length) {
                 $.when(
+
                     $.get(gituser.api + query, function (user) {
+                        // If there's no name, set it to username
+                        if (!user.name) {
+                            user.name = user.login;
+                        }
                         gituser.cache.user = user;
                     }).fail(function () {
-                        alert("error fetching user");
+                        var error = "Sorry, this username doesn't exist!";
+                        gituser.errors.messages.push(error);
                     }),
+
                     $.get(gituser.api + query + "/repos", function (repos) {
                         gituser.cache.repos = repos;
                     }).fail(function () {
-                        alert("error fetching repos");
+                        var error = "Sorry, there's been an error fetching repos for this username!";
+                        gituser.errors.messages.push(error);
+                        gituser.renderView(gituser.errors, "error.html", "#result-container");
                     })
+
                 ).then(function () {
-                    gituser.discoverFavourites();
+                    // Clear the input field
+                    $(input).val("");
+                    // Go through data to get what we need
+                    gituser.serialize();
                 });
+            } else {
+                var error = "Please enter a username!";
+                gituser.errors.messages.push(error);
+                gituser.renderView(gituser.errors, "error.html", "#result-container");
             }
         },
 
-        discoverFavourites: function () {
+        serialize: function () {
             var langs = [];
 
-            _.each(gituser.cache.repos, function (repo) {
+            if (gituser.cache.repos.length) {
+                _.each(gituser.cache.repos, function (repo) {
 
-                var existingLanguage = _.findWhere(langs, { name: repo.language });
+                    var existingLanguage = _.findWhere(langs, { name: repo.language });
 
-                if (!existingLanguage) {
-                    var lang = {};
-                    lang.name = repo.language;
-                    lang.quantity = 1;
+                    if (!existingLanguage) {
+                        var lang = {};
+                        lang.name = repo.language;
+                        lang.quantity = 1;
 
-                    langs.push(lang);
-                } else {
-                    existingLanguage.quantity++;
-                }
-            });
+                        langs.push(lang);
+                    } else {
+                        existingLanguage.quantity++;
+                    }
+                });
+            } else {
+                var error = "Sorry, this user doesn't appear to have any public repos :(";
+                gituser.errors.messages.push(error);
+                gituser.renderView(gituser.errors, "error.html", "#result-container");
+                return;
+            }
 
             gituser.cache.favLang = _.max(langs, function (lang) { return lang.quantity; });
             gituser.cache.otherLangs = _.filter(langs, function (lang) { return lang.name !== gituser.cache.favLang.name; });
-            console.log(gituser.cache);
 
-            gituser.renderView(gituser.cache, "result.html", "#result-container", false);
+            // Render the template
+            gituser.renderView(gituser.cache, "result.html", "#result-container");
         },
 
         renderView: function (data, template, el, callback) {
@@ -71,6 +97,9 @@
                 $(el).fadeIn("slow");
 
                 if (callback) { callback(); }
+
+                // Clear error cache
+                gituser.errors.messages = [];
             });
         }
     };
